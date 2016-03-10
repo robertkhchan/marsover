80b47e5c3b1d7a8009fa95e7b2ca52a0b75d8d12
Robert Chan <robertkhchan@gmail.com>
2016-03-10 01:13:31 -0500

    Completed happy path of Rover functionalities.

--- marsover/rover.py
+++ marsover/rover.py
@@ -6,31 +6,60 @@ Created on Mar 9, 2016
 from marsover.orientation import Orientation
 
 class Rover(object):
-
     
     def __init__(self, name, plateau):
         self.name = name
         self.plateau = plateau
+        self.movement = {
+            "L" : self.left,
+            "R" : self.right,
+            "M" : self.move
+        }
         
         
-    def setLanding(self, x, y, orientation):
-        
+    def setLanding(self, x, y, orientation):        
         if (x > self.plateau.borderX or y > self.plateau.borderY):
             raise RuntimeError("Landing outside of plateau")
         else:
             self.x = x
             self.y = y
-        
-        
+                
         try:
             self.orientation = Orientation[orientation]
         except KeyError:
             raise RuntimeError("Unsupported orientation")
         
     
-    
-    
-    
+    def setInstruction(self, instruction):
+        self.instruction = instruction
+        
+        
+    def run(self):        
+        if not hasattr(self, "x") or not hasattr(self, "y") or not hasattr(self, "orientation"): 
+            raise RuntimeError("Missing landing information")
+        if not hasattr(self, "instruction"): 
+            raise RuntimeError("Missing instruction")
+        
+        for c in self.instruction:
+            self.movement[c]()
+        
+        
+    def getDestination(self):    
+        return str(self.x) + ", " + str(self.y) + ", " + self.orientation.name
 
 
-
+    def left(self):
+        self.orientation = Orientation((self.orientation.value-1) % 4)
+         
+    def right(self):
+        self.orientation = Orientation((self.orientation.value+1) % 4)
+            
+    def move(self):
+        if self.orientation == Orientation.N:
+            self.y += 1
+        elif self.orientation == Orientation.E:
+            self.x += 1
+        elif self.orientation == Orientation.S:
+            self.y -= 1
+        elif self.orientation == Orientation.W:
+            self.x -= 1
--- marsover/test/TestRover.py
+++ marsover/test/TestRover.py
@@ -14,7 +14,7 @@ class TestRover(unittest.TestCase):
         p = Plateau(5,5)
         rover = Rover(name="Rover1", plateau=p)
         
-        self.assertEqual(rover.name, "Rover1", "Get name failed")
+        self.assertEqual(rover.name, "Rover1", "Set name failed")
         
         
     def testSetLanding(self):
@@ -22,9 +22,9 @@ class TestRover(unittest.TestCase):
         rover = Rover("Rover1", p)
         rover.setLanding(x=1, y=1, orientation="N")
         
-        self.assertEqual(rover.x, 1, "Get x failed")
-        self.assertEqual(rover.y, 1, "Get y failed")
-        self.assertEqual(rover.orientation, Orientation.N, "Get orientation failed")
+        self.assertEqual(rover.x, 1, "Set x failed")
+        self.assertEqual(rover.y, 1, "Set y failed")
+        self.assertEqual(rover.orientation, Orientation.N, "Set orientation failed")
         
     def testSetLanding_OutsidePlateau(self):
         p = Plateau(5,5)
@@ -39,3 +39,40 @@ class TestRover(unittest.TestCase):
         
         with self.assertRaises(RuntimeError):
             rover.setLanding(x=1, y=1, orientation="H")
+            
+    def testSetInstruction(self):
+        p = Plateau(5,5)
+        rover = Rover("Rover1", p)
+        rover.setInstruction("LMLMLMLMM")
+        
+        self.assertEqual(rover.instruction, "LMLMLMLMM", "Set instruction failed")
+        
+    def testRun1(self):
+        p = Plateau(5,5)
+        rover = Rover("Rover1", p)
+        rover.setLanding(x=1, y=1, orientation="N")
+        rover.setInstruction("LMLMLMLMM")
+        rover.run()
+        
+        self.assertEqual(rover.getDestination(), "1, 2, N", "Incorrect destination")
+        
+    def testRun2(self):
+        p = Plateau(5,5)
+        rover = Rover("Rover2", p)
+        rover.setLanding(x=3, y=3, orientation="E")
+        rover.setInstruction("MMRMMRMRRM")
+        rover.run()
+        
+        self.assertEqual(rover.getDestination(), "5, 1, E", "Incorrect destination")
+        
+    def testRun_WithMissingProperties(self):
+        p = Plateau(5,5)
+        rover = Rover("Rover1", p)
+        
+        with self.assertRaises(RuntimeError) as re: rover.run()
+        self.assertEqual("Missing landing information", str(re.exception))
+        
+        rover.setLanding(x=1, y=1, orientation="N")        
+        with self.assertRaises(RuntimeError) as re: rover.run()
+        self.assertEqual("Missing instruction", str(re.exception))
+        
